ba28db627a8ffebf0ce7949fc6c5c62c9c6aacd5
0E800 <0ld3nglish800@gmail.com>
2017-04-14 06:32:20 +0000

    modified:   less/extension.less 	modified:   src/Listeners/AddBBCode.php

--- src/Listeners/AddBBCode.php
+++ src/Listeners/AddBBCode.php
@@ -14,9 +14,9 @@ class AddBBCode
 
 
 $event->configurator->BBCodes->addCustom(
-            '[PBAR]{TEXT},{TEXT2},{COLOR},{COLOR2},{COLOR3},{NUMBER},{NUMBER2},{NUMBER3}[/PBAR]',
+            '[PBAR]{TEXT},{TEXT2},{COLOR},{COLOR2},{COLOR3},{NUMBER},{NUMBER2},{NUMBER3},{NUMBER4}[/PBAR]',
 '<h1>{TEXT}</h1>
-    <div class="arrow" style="border: {NUMBER}px solid {COLOR};border-radius:{NUMBER3}px">
+    <div class="arrow" style="border: {NUMBER}px solid {COLOR};border-radius:{NUMBER3}px;margin-bottom:{NUMBER4}px">
      <div class="arrow-status" style="width: {NUMBER2}%; background-color: {COLOR2};border-bottom-left-radius: {NUMBER3}px; border-top-left-radius: {NUMBER3}px;border-right: 0.5em solid {COLOR3}">
        <span class="arrow-pointer">{TEXT2}</span>
     </div>
